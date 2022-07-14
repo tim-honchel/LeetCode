c5914b3c9775578c6e22498e084c795566aeeb8c
Tim Honchel <tim.honchel@gmail.com>
2022-07-14 17:24:55 -0400

    new kata

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -118,10 +118,97 @@ namespace LeetCode
             return "";
         }
 
+        //public static bool isPalindrome(int x)
+        //{
+        //    var forward = Convert.ToString(x);
+        //    var chars = forward.ToCharArray();
+        //    Array.Reverse(chars);
+        //    var reverse = new string(chars);
+        //    Console.WriteLine(forward);
+        //    Console.WriteLine(reverse);
+        //    return (forward.Equals(reverse) ? true : false);
+ 
+        //}
+
+        public static bool isPalindrome(int x)
+        {
+            var number = Convert.ToString(x);
+            var digits = number.Length;
+            for (int i = 0; i < digits / 2; i++)
+            {
+                //Console.WriteLine($"{forward[i]}={forward[digits - i - 1]}");
+                if (number[i] != number[digits - i -1])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        public static string IntToRoman(int num)
+        {
+                var roman = "";
+                Sub(1000, "M");
+                Sub(900, "CM");
+                Sub(500, "D");
+                Sub(400, "CD");
+                Sub(100, "C");
+                Sub(90, "XC");
+                Sub(50, "L");
+                Sub(40, "XL");
+                Sub(10, "X");
+                Sub(9, "IX");
+                Sub(5, "V");
+                Sub(4, "IV");
+                Sub(1, "I");
+                return roman;
+
+                void Sub(int value, string numeral)
+                {
+                    var answer = "";
+                    while (num >= value)
+                    {
+                        roman += numeral;
+                        num -= value;
+                    }
+                }
+            
+        }
+
+        public static string ZigZagConvert(string s, int numRows)
+        {
+            if (numRows == 1)
+            {
+                return s;
+            }
+            var array = new string[numRows];
+            var direction = "down";
+            var row = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                row += direction == "down" ? 1 : -1;
+                array[row] += s[i];
+                if (row == 0 && direction == "up")
+                {
+                    direction = "down";
+                }
+                else if (row == numRows -1 && direction == "down")
+                {
+                    direction = "up";
+                }
+            }
+            var answer = array[0];
+            for (int j = 1; j < numRows; j++)
+            {
+                answer += array[j];
+            }
+            return answer;
+        }
+
         static void Main(string[] args)
         {
-            var test1 = "aacdefcaa";
-            var answer = LongestPalindrome(test1);
+            var test1 = 4300;
+            var answer = ZigZagConvert("PAYPALISHIRING", 3);
             Console.WriteLine(answer);
             Console.ReadKey();
         }
